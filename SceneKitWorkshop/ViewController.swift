346927e57338a49a10aabf565d2cfb1635783f96
Jurian de Cocq van Delwijnen <jurian.delwijnen@sogeti.com>
2018-12-03 21:31:39 +0100

    SceneKit - Assignment 3

--- SceneKitWorkshop/ViewController.swift
+++ SceneKitWorkshop/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import SceneKit
 import ARKit
 
-//----- Assignment 1; setup a plane that we can actually work with
 class Plane: SCNNode {
     let plane: SCNBox
     let planeNode: SCNNode
-    let planeHeight: Float = 0.1 // All SceneKit measurements are in meters, 0.1 is thus 10 centimeters
+    let planeHeight: Float = 0.1
     
     override init() {
         plane = SCNBox(width: 1, height: CGFloat(planeHeight), length: 1, chamferRadius: 0)
@@ -25,7 +24,7 @@ class Plane: SCNNode {
     }
     
     func show(_ visible: Bool) {
-        var materials = Array(repeating: SCNNode.material(for: UIColor.clear), count: 6) // 6 sides on the box
+        var materials = Array(repeating: SCNNode.material(for: UIColor.clear), count: 6)
         if visible {
             let color = UIColor.green.withAlphaComponent(0.7)
             materials[4] = SCNNode.material(for: color)
@@ -43,23 +42,9 @@ class Plane: SCNNode {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("Ignore this method. It's required but won't serve us now.")
     }
 }
-
-extension SCNNode {
-    class func material(for contents: Any) -> SCNMaterial {
-        let m = SCNMaterial()
-        m.diffuse.contents = contents
-        m.lightingModel = .physicallyBased
-        
-        return m
-    }
-}
-//-----------------------------------------------------------
-
-
-//----- Assignment 2; Let's add some balls to our pool table
 
 class Sphere: SCNNode {
     let sphere: SCNSphere
@@ -67,12 +52,7 @@ class Sphere: SCNNode {
     
     override init() {
         self.sphere = SCNSphere(radius: CGFloat(radius))
-//        sphere.materials = [SCNNode.material(for: UIColor.red)]
-        
-        //----- Assignment 4; different kind of pools balls and cleaning up
-        sphere.materials = [SCNNode.material(for: sharedImageIterator.next()!)]
-        //-----------------------------------------------------------
-        
+        sphere.materials = [SCNNode.material(for: UIColor.red)]
         super.init()
         self.geometry = sphere
         let shape = SCNPhysicsShape(geometry: sphere, options: nil)
@@ -80,7 +60,7 @@ class Sphere: SCNNode {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("Ignore this method. It's required but won't serve us now.")
     }
     
     func position(at transform: matrix_float4x4) {
@@ -88,34 +68,41 @@ class Sphere: SCNNode {
     }
 }
 
-//-----------------------------------------------------------
+// --------  Assignment 3  --------
+//  So far we're detecting the table and we can add balls to it
+//  Let's implement the ability to shoot our pool balls!
+//
+//  Shooting poolballs is actually quite simple, we only need to apply some force to our sphere
+//  Because of the dynamic shape, SceneKit will do all the actual heavy lifting!
+//
+//  1. In didTapView under our let location change our if let result to and else if let result
+//      the corrosponding if that should go with that looks like this:
+//      if let result = sceneView.hitTest(location, options: [:]).first, let sphere = result.node as? Sphere {
+//  We'll do a hit test and verify that we've actually tapped a sphere
+//  Define a let called force, we're going to use the result.localNormal for it
+//  LocalNormal is the angle the ball is tapped at but the force is define as going outwards
+//  Because we want to shoot the ball in the direction we're looking call .inverse() on the localNormal
+//  Inverse is not normally something part of SceneKit but I've prepared a method to use now, feel free to look at it
+//
+//  The tapping doesn't feel very satisfying yet, actually it's quite slow
+//  Let's call .multiply(factor: 2) on the inverted force
+//  Multiply isn't part of sceneKit either, it's just something I've prepared
+//      so you can play around with more and less force
+//
+//  Finally let's call sphere.physicsBody it's applyForce method, pass our force as an impulse
+//
+//
+//
+//  Run the app and notice that you can now tap the poolballs for a very satisfying shot!
+//  We can add poolballs now and shoot them as well, but our game doesn't look like pool yet
+//  For the next assignment we're going to clean it up and make our game look the part!
+// --------------------------------
 
 extension ViewController {
-
-    //----- Assignment 2; Let's add some balls to our pool table
-//    @objc func didTapView(sender: UITapGestureRecognizer) {
-//        let location = sender.location(in: sender.view)
-//        if let result = sceneView.hitTest(location, types: .existingPlane).first {
-//            let sphere = Sphere()
-//            sphere.position(at: result.worldTransform)
-//            sphere.position.y += sphere.radius
-//            sceneView.scene.rootNode.addChildNode(sphere)
-//        }
-//    }
-    //-----------------------------------------------------------
-    
-    
-    //----- Assignment 3; Let's see if we can shoot our pool balls
     
     @objc func didTapView(sender: UITapGestureRecognizer) {
         let location = sender.location(in: sender.view)
-        
-        if let result = sceneView.hitTest(location, options: [:]).first, let sphere = result.node as? Sphere {
-            // LocalNormal is the angle the ball is tapped and force going outwards
-            let force = result.localNormal.inverse().multiply(factor: 2)
-            sphere.physicsBody?.applyForce(force, asImpulse: true)
-        }
-        else if let result = sceneView.hitTest(location, types: .existingPlane).first {
+        if let result = sceneView.hitTest(location, types: .existingPlane).first {
             let sphere = Sphere()
             sphere.position(at: result.worldTransform)
             sphere.position.y += sphere.radius
@@ -123,10 +110,6 @@ extension ViewController {
         }
     }
     
-    //-----------------------------------------------------------
-
-    
-    //----- Assignment 1; setup a plane that we can actually work with
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if let planeAnchor = anchor as? ARPlaneAnchor {
             let plane = Plane()
@@ -141,26 +124,22 @@ extension ViewController {
             plane.update(for: planeAnchor)
         }
     }
-    //-----------------------------------------------------------
 }
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+// Everything below this line is boilerplate, just some code to get SceneKit up and running
 
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
     @IBOutlet var sceneView: ARSCNView!
     var planes: [ARPlaneAnchor: Plane] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         sceneView.delegate = self
         
-        //----- Assignment 4; different kind of pools balls and cleaning up
-//        sceneView.debugOptions = [.showPhysicsShapes]
-        //-----------------------------------------------------------
-
-        //        sceneView.debugOptions = [.showPhysicsShapes]
+        sceneView.debugOptions = [.showPhysicsShapes]
         sceneView.autoenablesDefaultLighting = true
         sceneView.automaticallyUpdatesLighting = true
         
@@ -179,8 +158,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
+    }
+}
+
+extension SCNNode {
+    class func material(for contents: Any) -> SCNMaterial {
+        let m = SCNMaterial()
+        m.diffuse.contents = contents
+        m.lightingModel = .physicallyBased
+        
+        return m
     }
 }
