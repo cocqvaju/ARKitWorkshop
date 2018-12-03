4d7199422872be29c0073d41fc3bc3590cfb7380
Jurian de Cocq van Delwijnen <jurian.delwijnen@sogeti.com>
2018-12-03 21:15:36 +0100

    SceneKit - Assignment 2

--- SceneKitWorkshop/ViewController.swift
+++ SceneKitWorkshop/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import SceneKit
 import ARKit
 
-//----- Assignment 1; setup a plane that we can actually work with
 class Plane: SCNNode {
     let plane: SCNBox
     let planeNode: SCNNode
-    let planeHeight: Float = 0.1 // All SceneKit measurements are in meters, 0.1 is thus 10 centimeters
+    let planeHeight: Float = 0.1
     
     override init() {
         plane = SCNBox(width: 1, height: CGFloat(planeHeight), length: 1, chamferRadius: 0)
@@ -25,7 +24,7 @@ class Plane: SCNNode {
     }
     
     func show(_ visible: Bool) {
-        var materials = Array(repeating: SCNNode.material(for: UIColor.clear), count: 6) // 6 sides on the box
+        var materials = Array(repeating: SCNNode.material(for: UIColor.clear), count: 6)
         if visible {
             let color = UIColor.green.withAlphaComponent(0.7)
             materials[4] = SCNNode.material(for: color)
@@ -43,44 +42,54 @@ class Plane: SCNNode {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("Ignore this method. It's required but won't serve us now.")
     }
 }
 
-extension SCNNode {
-    class func material(for contents: Any) -> SCNMaterial {
-        let m = SCNMaterial()
-        m.diffuse.contents = contents
-        m.lightingModel = .physicallyBased
-        
-        return m
-    }
-}
-//-----------------------------------------------------------
-
-
-//----- Assignment 2; Let's add some balls to our pool table
+// --------  Assignment 2  --------
+//  As promised we'll start adding some balls to our poolgame now
+//  I've prepared a second SCNNode type, the Sphere!
+//  Don't mind the positioning logic, we'll simply use it later
+//
+//  1. First off we're going to define a let sphere of type SCNSphere
+//  Also add a radius of type Float with value 0.025
+//  In the init method we're going to set self.sphere to a new SCNSphere with radisu CGFloat(radius)
+//  Also set the sphere.materials to an array of SCNNode.material and let's use UIColor.red for now
+//  Call super.init followed by setting self.geometry to sphere
+//  Define a let called shape and fill it with a SCNPhysicsShape(geometry: sphere and options nil
+//  Now set the physicsBody to a new SCNPhysicsBody but user type .dynamic this time and shape: shape
+//  Since we want our poolballs to be movable we've selected the dynamic type for this object
+//
+//  2. Now we've defined what a poolball for our game is, let's start adding them to the table
+//  Start in the didTapView method where we'll define a new let called location
+//      assign the sender.location(in: sender.view) to our newly defined location
+//  Now we need to check if we've actually tapped on something in our virtual world, do it as follows;
+//      if let result = sceneView.hitTest(location, types: .existingPlane).first {
+//  If the user has indeed tapped on a part of the virtual world we'll enter this block of code
+//  Let's define a new let called sphere and fill it with a new Sphere()
+//  Set the sphere.position at result.worldTransform, this is the calculated position in our virtual world
+//  Since the user taps a plane and we want to place our ball on top of the plane we should
+//      add the sphere.radius to our sphere.position.y
+//  Lastly, we should add our ball to the scene by calling sceneView.scene.rootNode.addChildNode(sphere)
+//
+//
+//
+//  Run the app and observe that if you tap on any of our green planes you'll add a red pool ball
+//  Since we've made our sphere shape a dynamic one they will automatically start moving if you place them
+//      on top or against each other
+//  Don't mind all the lines coming out of the balls, that's debug information for positioning elements
+//  We're missing quite a key component though, shooting the pool ball!
+//  We'll look into that for assignment 3
+// --------------------------------
 
 class Sphere: SCNNode {
-    let sphere: SCNSphere
-    let radius: Float = 0.025
     
     override init() {
-        self.sphere = SCNSphere(radius: CGFloat(radius))
-//        sphere.materials = [SCNNode.material(for: UIColor.red)]
-        
-        //----- Assignment 4; different kind of pools balls and cleaning up
-        sphere.materials = [SCNNode.material(for: sharedImageIterator.next()!)]
-        //-----------------------------------------------------------
-        
         super.init()
-        self.geometry = sphere
-        let shape = SCNPhysicsShape(geometry: sphere, options: nil)
-        self.physicsBody = SCNPhysicsBody(type: .dynamic, shape: shape)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("Ignore this method. It's required but won't serve us now.")
     }
     
     func position(at transform: matrix_float4x4) {
@@ -88,45 +97,12 @@ class Sphere: SCNNode {
     }
 }
 
-//-----------------------------------------------------------
-
 extension ViewController {
-
-    //----- Assignment 2; Let's add some balls to our pool table
-//    @objc func didTapView(sender: UITapGestureRecognizer) {
-//        let location = sender.location(in: sender.view)
-//        if let result = sceneView.hitTest(location, types: .existingPlane).first {
-//            let sphere = Sphere()
-//            sphere.position(at: result.worldTransform)
-//            sphere.position.y += sphere.radius
-//            sceneView.scene.rootNode.addChildNode(sphere)
-//        }
-//    }
-    //-----------------------------------------------------------
-    
-    
-    //----- Assignment 3; Let's see if we can shoot our pool balls
     
     @objc func didTapView(sender: UITapGestureRecognizer) {
-        let location = sender.location(in: sender.view)
         
-        if let result = sceneView.hitTest(location, options: [:]).first, let sphere = result.node as? Sphere {
-            // LocalNormal is the angle the ball is tapped and force going outwards
-            let force = result.localNormal.inverse().multiply(factor: 2)
-            sphere.physicsBody?.applyForce(force, asImpulse: true)
-        }
-        else if let result = sceneView.hitTest(location, types: .existingPlane).first {
-            let sphere = Sphere()
-            sphere.position(at: result.worldTransform)
-            sphere.position.y += sphere.radius
-            sceneView.scene.rootNode.addChildNode(sphere)
-        }
     }
     
-    //-----------------------------------------------------------
-
-    
-    //----- Assignment 1; setup a plane that we can actually work with
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if let planeAnchor = anchor as? ARPlaneAnchor {
             let plane = Plane()
@@ -141,26 +117,22 @@ extension ViewController {
             plane.update(for: planeAnchor)
         }
     }
-    //-----------------------------------------------------------
 }
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+// Everything below this line is boilerplate, just some code to get SceneKit up and running
 
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
     @IBOutlet var sceneView: ARSCNView!
     var planes: [ARPlaneAnchor: Plane] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         sceneView.delegate = self
         
-        //----- Assignment 4; different kind of pools balls and cleaning up
-//        sceneView.debugOptions = [.showPhysicsShapes]
-        //-----------------------------------------------------------
-
-        //        sceneView.debugOptions = [.showPhysicsShapes]
+        sceneView.debugOptions = [.showPhysicsShapes]
         sceneView.autoenablesDefaultLighting = true
         sceneView.automaticallyUpdatesLighting = true
         
@@ -179,8 +151,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
+    }
+}
+
+extension SCNNode {
+    class func material(for contents: Any) -> SCNMaterial {
+        let m = SCNMaterial()
+        m.diffuse.contents = contents
+        m.lightingModel = .physicallyBased
+        
+        return m
     }
 }
