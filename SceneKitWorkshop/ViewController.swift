10236451b70108ee9c978926f1fcd3059fb9fe35
Jurian de Cocq van Delwijnen <jurian.delwijnen@sogeti.com>
2018-12-03 21:50:48 +0100

    SceneKit - Complete solution

--- SceneKitWorkshop/ViewController.swift
+++ SceneKitWorkshop/ViewController.swift
@@ -10,22 +10,21 @@ import UIKit
 import SceneKit
 import ARKit
 
-//----- Assignment 1; setup a plane that we can actually work with
 class Plane: SCNNode {
     let plane: SCNBox
     let planeNode: SCNNode
-    let planeHeight: Float = 0.1 // All SceneKit measurements are in meters, 0.1 is thus 10 centimeters
+    let planeHeight: Float = 0.1
     
     override init() {
         plane = SCNBox(width: 1, height: CGFloat(planeHeight), length: 1, chamferRadius: 0)
         planeNode = SCNNode(geometry: plane)
         super.init()
         addChildNode(planeNode)
-        show(true)
+        show(false)
     }
     
     func show(_ visible: Bool) {
-        var materials = Array(repeating: SCNNode.material(for: UIColor.clear), count: 6) // 6 sides on the box
+        var materials = Array(repeating: SCNNode.material(for: UIColor.clear), count: 6)
         if visible {
             let color = UIColor.green.withAlphaComponent(0.7)
             materials[4] = SCNNode.material(for: color)
@@ -39,27 +38,12 @@ class Plane: SCNNode {
         let shape = SCNPhysicsShape(geometry: plane, options: nil)
         planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: shape)
         planeNode.position = SCNVector3(x: anchor.center.x, y: -planeHeight/2, z: anchor.center.z)
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("Ignore this method. It's required but won't serve us now.")
     }
 }
-
-extension SCNNode {
-    class func material(for contents: Any) -> SCNMaterial {
-        let m = SCNMaterial()
-        m.diffuse.contents = contents
-        m.lightingModel = .physicallyBased
-        
-        return m
-    }
-}
-//-----------------------------------------------------------
-
-
-//----- Assignment 2; Let's add some balls to our pool table
 
 class Sphere: SCNNode {
     let sphere: SCNSphere
@@ -67,12 +51,7 @@ class Sphere: SCNNode {
     
     override init() {
         self.sphere = SCNSphere(radius: CGFloat(radius))
-//        sphere.materials = [SCNNode.material(for: UIColor.red)]
-        
-        //----- Assignment 4; different kind of pools balls and cleaning up
         sphere.materials = [SCNNode.material(for: sharedImageIterator.next()!)]
-        //-----------------------------------------------------------
-        
         super.init()
         self.geometry = sphere
         let shape = SCNPhysicsShape(geometry: sphere, options: nil)
@@ -80,7 +59,7 @@ class Sphere: SCNNode {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("Ignore this method. It's required but won't serve us now.")
     }
     
     func position(at transform: matrix_float4x4) {
@@ -88,24 +67,7 @@ class Sphere: SCNNode {
     }
 }
 
-//-----------------------------------------------------------
-
 extension ViewController {
-
-    //----- Assignment 2; Let's add some balls to our pool table
-//    @objc func didTapView(sender: UITapGestureRecognizer) {
-//        let location = sender.location(in: sender.view)
-//        if let result = sceneView.hitTest(location, types: .existingPlane).first {
-//            let sphere = Sphere()
-//            sphere.position(at: result.worldTransform)
-//            sphere.position.y += sphere.radius
-//            sceneView.scene.rootNode.addChildNode(sphere)
-//        }
-//    }
-    //-----------------------------------------------------------
-    
-    
-    //----- Assignment 3; Let's see if we can shoot our pool balls
     
     @objc func didTapView(sender: UITapGestureRecognizer) {
         let location = sender.location(in: sender.view)
@@ -123,10 +85,6 @@ extension ViewController {
         }
     }
     
-    //-----------------------------------------------------------
-
-    
-    //----- Assignment 1; setup a plane that we can actually work with
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if let planeAnchor = anchor as? ARPlaneAnchor {
             let plane = Plane()
@@ -141,26 +99,21 @@ extension ViewController {
             plane.update(for: planeAnchor)
         }
     }
-    //-----------------------------------------------------------
 }
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+// Everything below this line is boilerplate, just some code to get SceneKit up and running
 
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
     @IBOutlet var sceneView: ARSCNView!
     var planes: [ARPlaneAnchor: Plane] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         sceneView.delegate = self
         
-        //----- Assignment 4; different kind of pools balls and cleaning up
-//        sceneView.debugOptions = [.showPhysicsShapes]
-        //-----------------------------------------------------------
-
-        //        sceneView.debugOptions = [.showPhysicsShapes]
         sceneView.autoenablesDefaultLighting = true
         sceneView.automaticallyUpdatesLighting = true
         
@@ -179,8 +132,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
+    }
+}
+
+extension SCNNode {
+    class func material(for contents: Any) -> SCNMaterial {
+        let m = SCNMaterial()
+        m.diffuse.contents = contents
+        m.lightingModel = .physicallyBased
+        
+        return m
     }
 }
